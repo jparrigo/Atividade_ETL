1aec78f5e77153c329dd643235a7d1502dd4aebe
Enrico <enricofarina667@gmail.com>
2024-06-18 19:20:06 -0300

    commit enrico 2

--- backend-operacional/src/index.ts
+++ backend-operacional/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import clienteR from "./routers/cliente-router";
 import acaoRouter from "./routers/acao.routes";
+import ordemRouter from "./routers/ordem.routes";
 
 const PORT = 4000;
 const HOSTNAME = "http://localhost";
@@ -13,6 +14,8 @@ app.use(express.json())
 app.use("/api", clienteR);
 
 app.use("/acao", acaoRouter);
+
+app.use("/ordem", ordemRouter);
 
 // Start server
 app.listen(PORT, () => {
